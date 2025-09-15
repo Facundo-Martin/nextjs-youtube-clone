ceacd18109e066ac26dc12a12db987efe8245293
Facundo Martin <facundomartin98@gmail.com>
2025-09-15 13:30:06 -0300

    chore(api): rm deprecated posts endpoints & queries

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,6 @@ import { api } from "@/trpc/server";
 import Image from "next/image";
 
 export default async function Home() {
-  void api.post.getLatest.prefetch();
-
   return (
     <div>
       <Image src="/logo.svg" height={50} width={50} alt="Logo" />
