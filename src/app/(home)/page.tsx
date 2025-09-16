f925219a7d86d0a3b28c79db71b2405fb64785ef
Facundo Martin <facundomartin98@gmail.com>
2025-09-16 12:54:47 -0300

    feat(FE): integrate api route

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,21 @@
-import { api } from "@/trpc/server";
+import { api, HydrateClient } from "@/trpc/server";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
+  const user = await auth();
+  void api.user.get.prefetch({ clerkId: user.userId! });
+
   return (
-    <div>
-      <Image src="/logo.svg" height={50} width={50} alt="Logo" />
-      <p className="text-xl font-semibold tracking-tight">Home page</p>
-    </div>
+    <HydrateClient>
+      <Suspense>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <Image src="/logo.svg" height={50} width={50} alt="Logo" />
+          <p className="text-xl font-semibold tracking-tight">Home page</p>
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { users } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+
+export const userRouter = createTRPCRouter({
+  get: publicProcedure
+    .input(z.object({ clerkId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const [user] = await ctx.db
+        .select({
+          id: users.id,
+          clerkId: users.clerkId,
+          name: users.name,
+          bannerUrl: users.bannerUrl,
+          bannerKey: users.bannerKey,
+          imageUrl: users.imageUrl,
+          createdAt: users.createdAt,
+          updatedAt: users.updatedAt,
+        })
+        .from(users)
+        .where(eq(users.clerkId, input.clerkId))
+        .limit(1);
+
+      return user;
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const allUsers = await ctx.db.select().from(users);
+    return allUsers;
+  }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      //   await ctx.db.insert(users).values({
+      //     name: input.name,
+      //   });
+    }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    return null;
+  }),
+});
