c64295f66d185e0c91236e0303de2d048596eaf0
Facundo Martin <facundomartin98@gmail.com>
2025-09-18 15:46:38 -0300

    feat: create videos section with infinite query

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,3 +1,13 @@
-export default function StudioPage() {
-  return <div>StudioPage</div>;
+import { DEFAULT_QUERY_LIMIT } from "@/lib/constants";
+import { StudioView } from "@/modules/studio/ui/components/views/studio-view";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function StudioPage() {
+  void api.video.getAll.prefetchInfinite({ limit: DEFAULT_QUERY_LIMIT });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 }
---
+++ src/modules/studio/ui/components/sections/videos-section.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import Link from "next/link";
+import { Suspense } from "react";
+import { format } from "date-fns";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { api } from "@/trpc/react";
+import { DEFAULT_QUERY_LIMIT } from "@/lib/constants";
+
+export const VideosSection = () => {
+  return (
+    <Suspense fallback={<VideosSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSkeleton = () => {
+  return (
+    <>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[510px] pl-6">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="pr-6 text-right">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <TableRow key={index}>
+                <TableCell className="pl-6">
+                  <div className="flex items-center gap-4">
+                    <Skeleton className="h-20 w-36" />
+                    <div className="flex flex-col gap-2">
+                      <Skeleton className="h-4 w-[100px]" />
+                      <Skeleton className="h-3 w-[150px]" />
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-20" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-16" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-24" />
+                </TableCell>
+                <TableCell className="text-right">
+                  <Skeleton className="ml-auto h-4 w-12" />
+                </TableCell>
+                <TableCell className="text-right">
+                  <Skeleton className="ml-auto h-4 w-12" />
+                </TableCell>
+                <TableCell className="pr-6 text-right">
+                  <Skeleton className="ml-auto h-4 w-12" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [videos, query] = api.video.getAll.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_QUERY_LIMIT,
+    },
+    {
+      getNextPageParam: (data) => data.nextCursor,
+    },
+  );
+
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[510px] pl-6">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="pr-6 text-right">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  prefetch
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell className="pl-6">Video thumbnail</TableCell>
+                    <TableCell>visibility</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell className="truncate text-sm">
+                      {format(new Date(video.createdAt), "d MMM yyyy")}
+                    </TableCell>
+                    <TableCell className="text-right text-sm">
+                      Views count
+                    </TableCell>
+                    <TableCell className="text-right text-sm">
+                      Comments count
+                    </TableCell>
+                    <TableCell className="pr-6 text-right text-sm">
+                      Likes count
+                    </TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
