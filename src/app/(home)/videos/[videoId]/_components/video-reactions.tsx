2d2925bb46b93005a2bacad870312538470b7180
Facundo Martin <facundomartin98@gmail.com>
2025-10-24 17:28:29 -0300

    refactor: handle unauthenticated case more elegantly rather than have mutation fail

--- src/app/(home)/videos/[videoId]/_components/video-reactions.tsx
+++ src/app/(home)/videos/[videoId]/_components/video-reactions.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { api, type RouterOutputs } from "@/trpc/react";
-import { useClerk } from "@clerk/nextjs";
+import { useClerk, useUser } from "@clerk/nextjs";
 import { ThumbsUpIcon } from "lucide-react";
 import { toast } from "sonner";
 
@@ -20,25 +20,31 @@ export const VideoReactions = ({
   viewerReaction,
 }: Props) => {
   const clerk = useClerk();
+  const { isSignedIn } = useUser();
   const utils = api.useUtils();
 
   const reactToVideo = api.videoReactions.toggleReaction.useMutation({
     onSuccess: () => utils.video.getPublicVideo.invalidate({ videoId }),
-    onError: (error) => {
-      if (error.data?.code === "UNAUTHORIZED") {
-        clerk.openSignIn();
-      } else {
-        toast.error("Something went wrong");
-      }
+    onError: () => {
+      toast.error("Something went wrong");
     },
   });
+
+  const onReact = (type: "like" | "dislike") => {
+    if (!isSignedIn) {
+      clerk.openSignIn();
+      return;
+    }
+
+    reactToVideo.mutate({ videoId, type });
+  };
 
   return (
     <div className="flex flex-none items-center">
       <Button
         variant="secondary"
         className="gap-2 rounded-l-full rounded-r-none pr-4"
-        onClick={() => reactToVideo.mutate({ videoId, type: "like" })}
+        onClick={() => onReact("like")}
       >
         <ThumbsUpIcon
           className={cn("size-5", { "fill-black": viewerReaction === "like" })}
@@ -49,10 +55,10 @@ export const VideoReactions = ({
       <Button
         variant="secondary"
         className="rounded-l-none rounded-r-full pl-3"
-        onClick={() => reactToVideo.mutate({ videoId, type: "dislike" })}
+        onClick={() => onReact("dislike")}
       >
         <ThumbsUpIcon
-          className={cn("size-5", {
+          className={cn("size-5 rotate-180", {
             "fill-black": viewerReaction === "dislike",
           })}
         />
