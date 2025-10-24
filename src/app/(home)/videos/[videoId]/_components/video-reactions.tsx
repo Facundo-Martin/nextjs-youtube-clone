7158554e070bc27ac87a8cb659b12f7e6a2c3ec7
Facundo Martin <facundomartin98@gmail.com>
2025-10-24 17:36:56 -0300

    refactor: add optimistic updates for video likes and dislikes (feels much faster now!)

--- src/app/(home)/videos/[videoId]/_components/video-reactions.tsx
+++ src/app/(home)/videos/[videoId]/_components/video-reactions.tsx
@@ -23,10 +23,83 @@ export const VideoReactions = ({
   const { isSignedIn } = useUser();
   const utils = api.useUtils();
 
+  // Note: Optimistic updates following: https://tanstack.com/query/v4/docs/framework/react/guides/optimistic-updates#updating-a-list-of-todos-when-adding-a-new-todo
   const reactToVideo = api.videoReactions.toggleReaction.useMutation({
-    onSuccess: () => utils.video.getPublicVideo.invalidate({ videoId }),
-    onError: () => {
+    onMutate: async ({ type }) => {
+      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+      await utils.video.getPublicVideo.cancel({ videoId });
+
+      // Snapshot the previous value
+      const previousData = utils.video.getPublicVideo.getData({ videoId });
+
+      // Optimistically update to the new value
+      if (previousData) {
+        utils.video.getPublicVideo.setData({ videoId }, (old) => {
+          if (!old) return old;
+
+          let newLikeCount = old.likeCount;
+          let newDislikeCount = old.dislikeCount;
+          let newViewerReaction = old.viewerReaction;
+
+          // Handle like button click
+          if (type === "like") {
+            if (viewerReaction === "like") {
+              // Remove like
+              newLikeCount--;
+              newViewerReaction = null;
+            } else if (viewerReaction === "dislike") {
+              // Switch from dislike to like
+              newDislikeCount--;
+              newLikeCount++;
+              newViewerReaction = "like";
+            } else {
+              // Add like
+              newLikeCount++;
+              newViewerReaction = "like";
+            }
+          }
+
+          // Handle dislike button click
+          else if (type === "dislike") {
+            if (viewerReaction === "dislike") {
+              // Remove dislike
+              newDislikeCount--;
+              newViewerReaction = null;
+            } else if (viewerReaction === "like") {
+              // Switch from like to dislike
+              newLikeCount--;
+              newDislikeCount++;
+              newViewerReaction = "dislike";
+            } else {
+              // Add dislike
+              newDislikeCount++;
+              newViewerReaction = "dislike";
+            }
+          }
+
+          return {
+            ...old,
+            likeCount: newLikeCount,
+            dislikeCount: newDislikeCount,
+            viewerReaction: newViewerReaction,
+          };
+        });
+      }
+
+      // Return a context object with the snapshotted value
+      return { previousData };
+    },
+    // If the mutation fails,
+    // use the context returned from onMutate to roll back
+    onError: (_, __, context) => {
+      if (context?.previousData) {
+        utils.video.getPublicVideo.setData({ videoId }, context.previousData);
+      }
       toast.error("Something went wrong");
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      void utils.video.getPublicVideo.invalidate({ videoId });
     },
   });
 
